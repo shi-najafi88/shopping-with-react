7de08e4444bb1de6506e4ed757fb1a2ab7f2f3b8
shirin najafi <shinajafi401@gmail.com>
2023-01-26 18:22:38 +0330

    first commit

---
+++ src/navbare/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./nav.css"
+
+const Nav = ()=> {
+  return (
+    <div>
+        <p className='navContent'>React Shopping Cart</p>
+    </div>
+  )
+}
+
+export default Nav
